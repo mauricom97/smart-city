4c0722fb40166645a9d7ef78cf46b77db71cffa4
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2022-04-13 17:35:23 -0300

    Implmentação de logica e rota dos sensores

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const { Board, Sensor } = require("johnny-five");
 const board = new Board({port: "COM4"});
 
+let whaterSensors = new Array()
 board.on("ready", () => {
-    const sensor = new Sensor("A0")
-  
-    sensor.on("change", function () {
-      console.log(this.value)
-    })
+  const sensor = new Sensor("A0")
+  let whaterLevel = null
+  sensor.within([0, 400], function () {
+    whaterLevel = {emergency: false, message: "The water level is low."}
+    console.log(whaterLevel)
   })
+  sensor.within([401, 800], function () {
+    whaterLevel = {emergency: false, message: "The water level is normal."}
+    console.log(whaterLevel)
+  })
+  sensor.within([801, 1800], function () {
+    whaterLevel = {emergency: false, message: "The water level is high."}
+    console.log(whaterLevel)
+  })
+})
+
+console.log(whaterSensors)
