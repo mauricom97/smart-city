f1e32f4a7bc708452987d6e0c58ccd53ba778bae
Mauricio <mauricionunesdasilvanunes@gmail.com>
2022-05-05 20:15:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const { Board, Sensor } = require("johnny-five");
-const board = new Board({port: "COM4"});
+const { Board, Sensor } = require("johnny-five")
+const board = new Board({port: "COM3"})
 
 board.on("ready", () => {
-    const sensor = new Sensor("A0")
-  
-    sensor.on("change", function () {
-      console.log(this.value)
-    })
+  const sensor = new Sensor("A0")
+  let outOfCoffee = false
+
+  sensor.within([0, 299], () => {
+    if (!outOfCoffee) {
+      outOfCoffee = true
+      console.log("O nivel da agua esta normal")
+    }
   })
+
+  sensor.within([300, 500], () => {
+    if (outOfCoffee) {
+      outOfCoffee = false
+      console.log("O nivel da aguá está alto!!")
+    }
+  })
+})
